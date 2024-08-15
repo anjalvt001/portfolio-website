f9a01bfefb95f9af06e818a32cfaa44624b1814a
anjal <anjalvt001@gmail.com>
2024-08-16 00:10:01 +0530

    project updated

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const circles = document.querySelectorAll('.circle');
+
+    circles.forEach(circle => {
+        const percentage = circle.getAttribute('data-percentage');
+        circle.style.setProperty('--percentage', percentage);
+    });
+});
+
